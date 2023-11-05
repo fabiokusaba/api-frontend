96c62f20d239ea0becd11ffcaf75bfa24ec98928
Fabio Kusaba <fabinhokb44@gmail.com>
2023-11-05 15:50:37 -0300

    Add: listagem de clientes

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Cliente } from '../modelo/Cliente';
+import { ClienteService } from '../servico/cliente.service';
 
 @Component({
   selector: 'app-principal',
@@ -9,5 +11,18 @@ export class PrincipalComponent {
 
   //Variável para visibilidade dos botões
   btnCadastro: boolean = true;
+
+  //JSON de clientes
+  clientes: Cliente[] = [];
+
+  //Construtor -> injeção de dependência -> acesso ao nosso ClienteService
+  constructor(private servico: ClienteService) {
+  }
+
+  //Método selecionar clientes
+  selecionar(): void {
+    this.servico.selecionar()
+      .subscribe(retorno => this.clientes = retorno);
+  }
 
 }
