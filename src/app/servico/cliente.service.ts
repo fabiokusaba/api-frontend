9fd65aa48594ca546cf27ca6fbd42ec892d73274
Fabio Kusaba <fabinhokb44@gmail.com>
2023-11-05 15:44:16 -0300

    Add: método selecionar clientes

--- src/app/servico/cliente.service.ts
+++ src/app/servico/cliente.service.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cliente } from '../modelo/Cliente';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
-  
+
   //URL da API
   private URL_API: string = 'http://localhost:8080/clientes';
 
   //Construtor -> injetando o HttClient -> responsável pelas requisições à nossa API
   constructor(private http: HttpClient) { }
+
+  //Método para selecionar todos os clientes
+  selecionar(): Observable<Cliente[]> {
+    return this.http.get<Cliente[]>(this.URL_API);
+  }
 }
