918004b5e31447dab2d4716cb75ce8e32175e31c
Fabio Kusaba <fabinhokb44@gmail.com>
2023-11-05 16:11:20 -0300

    Add: ngModel para trabalhar com input de formulário

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -9,6 +9,9 @@ import { ClienteService } from '../servico/cliente.service';
 })
 export class PrincipalComponent {
 
+  //Objeto do tipo Cliente
+  cliente = new Cliente();
+
   //Variável para visibilidade dos botões
   btnCadastro: boolean = true;
 
